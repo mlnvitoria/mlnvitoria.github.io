4904518332c97c7b90ce4babff685a0e37b0be8a
Vitoria Mendes <vitoriamendes95@pm.me>
2026-06-04 18:54:21 -0400

    Skills completed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,30 +1,38 @@
 function Skills() {
     return <>
-    <h2>My Skills</h2>
-        <p>Front-end</p>
-        <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>Vue.js</li>
-            <li>AngularJS</li>
-        </ul>
-        <p>Back-end</p>
-        <ul>
-            <li>PHP [Laravel, CodeIgniter, Magento]</li>
-            <li>Node.js [Express]</li>
-            <li>APIs REST</li>
-            <li>SQL [Relational Databases: MySQL, Oracle, PostgreSQL</li>
-        </ul>
-        <p>Tools</p>
-        <ul>
-            <li>Git</li>
-            <li>Agile/Kanban</li>
-            <li>Postman</li>
-            <li>Visual Studio Code</li>
-        </ul>
+        <h3 className='text-lg font-bold mb-2'>My Skills</h3>
+        <div className="grid grid-cols-3 gap-4 mb-4">
+            <section className="p-4 bg-neutral-900 border-2 border-neutral-950 rounded-2xl align-middle">
+                <p className="font-bold">Front-end</p>
+                <ul className="ml-6 list-[square]">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>React</li>
+                    <li>Vue.js</li>
+                    <li>AngularJS</li>
+                </ul>
+            </section>
+            <section className="p-4 bg-neutral-900 border-2 border-neutral-950 rounded-2xl align-middle">
+                <p className="font-bold">Back-end</p>
+                <ul className="ml-6 list-[square]">
+                    <li>PHP [Laravel, CodeIgniter, Magento]</li>
+                    <li>Node.js [Express]</li>
+                    <li>APIs REST</li>
+                    <li>SQL [MySQL, Oracle, PostgreSQL]</li>
+                </ul>
+            </section>
+            <section className="p-4 bg-neutral-900 border-2 border-neutral-950 rounded-2xl align-middle">
+                <p className="font-bold">Tools and Methodologies</p>
+                <ul className="ml-6 list-[square]">
+                    <li>Git</li>
+                    <li>Agile/Kanban</li>
+                    <li>Postman</li>
+                    <li>Visual Studio Code</li>
+                </ul>
+            </section>
+        </div>
     </>;
 }
 
